a5d82f0ce4fd662ce969f5ed4090fd88a7268a27
vanshaj behl <vanshajbehl96@gmail.com>
2016-06-22 12:45:53 +0000

    changes in slideshw

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,4 @@
-var frontBackground = ["https://unsplash.com/photos/7u6Jmwg2ZQ0/download","https://unsplash.com/photos/qmZF9CptLKs/download","https://unsplash.com/photos/k42aEvMwUGA/download","https://unsplash.com/photos/h4bBVo_CpqQ/download","https://unsplash.com/photos/F_J14NKUqLQ/download"];
+var frontBackground = ["https://unsplash.com/photos/7u6Jmwg2ZQ0/download","https://unsplash.com/photos/h4bBVo_CpqQ/download","https://unsplash.com/photos/F_J14NKUqLQ/download"];
 var i=1;
 function slideshow(){
     $(".jumbotron.front").css("background-image","url("+frontBackground[i]+")");
@@ -6,6 +6,6 @@ function slideshow(){
     if (i >= frontBackground.length) {
         i = 0;
     }
-    setTimeout(slideshow,9000);
+    setTimeout(slideshow,5000);
 }
 slideshow();
